4bcaaf509df7466d3b3061bf765dc01e8e8fe99b
Joao Almeida <176084410+04ramalmeida@users.noreply.github.com>
2024-12-25 15:07:45 +0000

    Add entities

---
+++ 04LibraryApi/Data/Entities/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace _04LibraryApi.Data.Entities;
+
+public class Book : IEntity
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    
+    public string Author { get; set; }
+
+    public string Genre { get; set; }
+    
+    public string Publisher { get; set; }
+    
+    public DateTime PublishDate { get; set; }
+    
+    public string ImageURL { get; set; }
+}
---
+++ 04LibraryApi/Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace _04LibraryApi.Data.Entities;
+
+public class User : IEntity
+{
+    public int Id { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public DateTime CreatedOn { get; set; }
+    
+    public string ImageUrl { get; set; }
+    
+    public ICollection<Book> Library { get; set; }    
+}
