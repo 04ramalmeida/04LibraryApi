7c5f70784eb40b5ef011b369290f69328aeca1ea
Joao Almeida <176084410+04ramalmeida@users.noreply.github.com>
2025-01-12 11:20:58 +0000

    Change library classes (2)

---
+++ 04LibraryApi/Data/Entities/Library.cs
@@ -0,0 +1,9 @@
+﻿namespace _04LibraryApi.Data.Entities;
+
+public class Library : IEntity
+{
+    public int Id { get; set; }
+    public required string UserId { get; set; }
+    
+    public ICollection<LibraryEntry> Books { get; set; }
+}
---
+++ 04LibraryApi/Data/Entities/LibraryEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace _04LibraryApi.Data.Entities;
+
+public class LibraryEntry : IEntity
+{
+    public int Id { get; set; }
+    
+    public int LibraryId { get; set; }
+    
+    public int BookId { get; set; }
+}
